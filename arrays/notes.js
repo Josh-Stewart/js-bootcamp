94afca894c4743a9603deec67e5be000b8af9a23
Josh Stewart <Josh-Stewart@users.noreply.github.com>
2018-04-21 22:02:01 +0800

    Arrays #1

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,6 +1,31 @@
 const notes = ['Note 1','Note 2','Note 3']
 
-console.log(notes.length)
+// notes.pop()
+// notes.push('My new note')
 
-console.log(notes[0])
-console.log(notes[notes.length - 1])
+// console.log(notes.shift())
+// notes.unshift('My first note')
+
+// notes.splice(1, 1, 'New second item')
+
+notes[2] = 'New note 3'
+
+notes.forEach(function (item, index) {
+    console.log(index)
+    console.log(item)
+})
+
+console.log(notes.length)
+console.log(notes)
+
+
+// Counting... 1
+for (let i = 0; i <= 2; i++) {
+    console.log(i)
+}
+
+for (let i=0; i < notes.length; i++) {
+    console.log(notes[i])
+}
+
+//console.log(notes.indexOf('note 2'))
--- arrays/todo.js
+++ arrays/todo.js
@@ -4,6 +4,21 @@
 
 const todoList = ['learn js', 'learn react', 'learn AWS', 'study maths', 'get a real job']
 
+// console.log(`Todo: ${todoList[0]}`)
+// console.log(`Todo: ${todoList[todoList.length-2]}`)
+
+// todoList.splice(2,1)
+// todoList.push('New item')
+// todoList.shift()
+
 console.log(`You have ${todoList.length} todos`)
-console.log(todoList[0])
-console.log(todoList[todoList.length-2])
+
+// todoList.forEach(function (item, index) {
+//     console.log(`${index + 1}. ${item}`)
+// })
+
+for (let i=0; i < todoList.length; i++) {
+    const num = i + 1
+    const todo = todoList[i]
+    console.log(`${num}. ${todo}`)
+}
