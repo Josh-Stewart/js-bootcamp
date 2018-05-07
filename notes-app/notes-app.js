094a56c081ec0ecdb593c277e2cd82e24d134256
Josh Stewart <Josh-Stewart@users.noreply.github.com>
2018-05-07 23:16:20 +0800

    commit changes before starting for the night

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,3 +1,14 @@
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to mars'
+}, {
+    title: 'House mods',
+    body: 'Remove tree from garden'
+}, {
+    title: 'Habbits to work on',
+    body: 'Coding argh!'
+}]
+
 // DOM
 
 // Query and remove 1
@@ -7,7 +18,13 @@
 // Query all and remove
 const ps = document.querySelectorAll('p')
 ps.forEach(function (p) {
-    p.textContent = '****'
-    //console.log(p.textContent)
-    //p.remove()
+
+    //p.textContent = '****' //censor content
+    //console.log(p)
+    p.remove()
 })
+
+// Add new element
+const newParagraph = document.createElement('p')
+newParagraph.textContent = 'This is a new element from Javascript'
+document.querySelector('body').appendChild(newParagraph)
---
+++ todo-app/todo-app.js
@@ -0,0 +1,28 @@
+const todos = [{
+    text: 'learn js',
+    completed: true
+}, {
+    text: 'learn react',
+    completed: false
+}, {
+    text: 'learn AWS',
+    completed: false
+}, {
+    text: 'study maths',
+    completed: false
+}, {
+    text: 'get a real job',
+    completed: true
+}]
+
+// you have N todos left (p element)
+
+// add a p for each todo above
+
+// const allParagraphs = document.querySelectorAll('p')
+
+// allParagraphs.forEach(function (p) {
+//     if (p.textContent.toLowerCase().includes('learn')) {
+//         p.remove()
+//     }
+// })
